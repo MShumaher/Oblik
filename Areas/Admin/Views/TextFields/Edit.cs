e94510363057bf0c5e89be5ca2c887da9e20f0e4
vitaliy.gunner <vitaliy.gunner@gmail.com>
2021-05-27 21:50:08 +0300

    adapt size images; load img; add and update services

---
+++ Areas/Admin/Views/TextFields/Edit.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oblik.Areas.Admin.Views.TextFields
+{
+    public class Edit : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
