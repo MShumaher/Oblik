a9a91b38fc870c8c51a8ca419983685ec6f7d1a0
vitaliy.gunner <vitaliy.gunner@gmail.com>
2021-06-05 17:35:54 +0300

    add PatientController

---
+++ Controllers/PatientsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Oblik.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oblik.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private readonly DataManager dataManager;
+
+        public PatientsController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+        public IActionResult Index(Guid id)
+        {
+            if (id != default)
+            {
+                return View("Show", dataManager.Patients.GetPatientById(id));
+            }
+            //TODO 
+            ViewBag.TextField = dataManager.TextFields.GetTextFieldByCodeWord("PagePatients");
+            return View(dataManager.Patients.GetPatients());
+        }
+    }
+}
